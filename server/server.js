ddd5af6414a4cf85feb1611ee87f2b7ad79400c0
Mpass <121707487+ME942082@users.noreply.github.com>
2025-03-21 14:22:43 +0800

    server problem

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
+/*import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import fs from "fs";
+import fs from "fs";*/
 import mongoose from "mongoose";
 import { User } from "./models/UserSchema.js";
 import { router as UserRoutes } from "./routes/UserRoutes.js";
@@ -13,11 +13,11 @@ import session from "express-session";
 import MongoStore from "connect-mongo";
 
 // 獲取當前檔案的路徑
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+/*const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);*/
 
 // 動態解析 .env 的絕對路徑
-const envPath = path.resolve(__dirname, "../.env");
+/*const envPath = path.resolve(__dirname, "../.env");
 console.log("Loading .env from:", envPath);
 
 // 檢查 .env 檔案是否存在
@@ -29,7 +29,7 @@ if (!fs.existsSync(envPath)) {
 const result = dotenv.config({ path: envPath });
 if (result.error) {
   throw new Error(`Failed to load .env: ${result.error.message}`);
-}
+}*/
 
 const DBURL = process.env.DB_URL;
 if (!DBURL) {
@@ -64,7 +64,7 @@ store.on("error", function (e) {
 });
 
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT || 5000;
 const sessionConfig = {
   store,
   name: "session",
