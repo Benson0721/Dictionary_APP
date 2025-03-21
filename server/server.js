57ba8a9e0890aec3adf033ccc52bc5724f5b9439
Mpass <121707487+ME942082@users.noreply.github.com>
2025-03-21 14:28:16 +0800

    server problem

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-/*import dotenv from "dotenv";
+import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import fs from "fs";*/
+import fs from "fs";
 import mongoose from "mongoose";
 import { User } from "./models/UserSchema.js";
 import { router as UserRoutes } from "./routes/UserRoutes.js";
