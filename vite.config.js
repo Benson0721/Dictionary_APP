356de13d249aa86647b1b6a55cc1dd0c0181b518
Mpass <121707487+ME942082@users.noreply.github.com>
2025-03-27 15:50:39 +0800

    add scss embedded and add margin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-
+import { NodePackageImporter } from "sass-embedded";
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
