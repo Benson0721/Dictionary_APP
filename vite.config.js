7ca644af80d8c08ade9e839b15168202fc082a26
Mpass <121707487+ME942082@users.noreply.github.com>
2025-03-29 16:15:49 +0800

    complate everythings

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { NodePackageImporter } from "sass-embedded";
+
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [react(), nodePolyfills()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler", // 使用 sass-embedded 的現代編譯器 API
-        importers: [new NodePackageImporter()], // 支持從 node_modules 導入
-      },
-    },
-  },
   build: {
     outDir: "./dist", // 將構建輸出到 DICTIONARY APP/dist/
     emptyOutDir: true, // 清空目標目錄
